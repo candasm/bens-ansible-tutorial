40225787e98fdc6973a70178f1c67ef33d7eab57
Candas Minareci <candas@minareci.net>
2017-04-21 17:11:13 +0300

    part 4

---
+++ roles/appserver/files/testapp.py
@@ -0,0 +1,30 @@
+# sample.py
+import falcon
+import MySQLdb
+import os
+
+class QuoteResource:
+
+	def on_get(self, req, resp):
+		"""Handlers GET requests """
+
+		resp.content_type = 'text/html'
+
+		try:
+			db = MySQLdb.connect(host="dbserver",
+								 user="ansibletutorial",
+								 passwd=os.environ['databasepassword'],
+								 db="ansibletutorial")
+		except:
+			resp.body = '<html><head><title>Error</title><body><h1>Database not available!</h1></body></html>'
+			return
+
+		cur = db.cursor()
+		cur.execute("SELECT quote, author, FROM quotes ORDER BY RAND() LIMIT 1")
+		data = cur.fetchone()
+
+		resp.body = '<html><head><title>Quote of the moment</title></head><body>h1{}</h1>-- {}</body></html>'.format(data[0], data[1])
+
+
+app = falcon.API()
+app.add_route('/', QuoteResource())
